2e8c67bcfd6fe3aab129838b0929f699fec9c9c9
Anton Cherry <39146381+ztxone@users.noreply.github.com>
2021-09-06 23:25:59 +0200

    Modal finished

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex flex-col items-center justify-center">
+    <h1 class="text-2xl font-bold">{{ title }}</h1>
+    <teleport to=".modals">
+      <Modal v-if="showModal" class="z-10" @close="toggleModal">
+        <h1
+          :class="{ ['text-white' + ' ' + 'bg-gray-800']: theme === 'dark' }"
+          class="text-xl"
+        >
+          First App
+        </h1>
+        <p>modal content</p>
+        <template v-slot:links>
+          <a href="#" class="p-4 bg-red-200 rounded-xl">Sing Up</a>
+          <a href="#" class="p-4 bg-red-200 rounded-xl">More info</a>
+        </template>
+      </Modal>
+    </teleport>
+
+    <button @click="toggleModal" class="rounded-xl bg-red-100 p-4">
+      Open Modal
+    </button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "First App",
+      showModal: false,
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add("active")
+      this.$refs.name.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    @click.self="closeModal"
+    class="backdrop top-0 fixed bg-black opacity-50 w-full h-full"
+  >
+    <div class="modal w-96 p-5 my-24 mx-auto bg-white rounded-xl text-center">
+      <slot></slot>
+      <div class="flex justify-evenly py-10">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["header", "theme"],
+  methods: {
+    closeModal() {
+      this.$emit("close")
+    },
+  },
+}
+</script>
